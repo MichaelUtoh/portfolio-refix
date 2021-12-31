9818c195095c64357e216c8bf34d045bfbf1a086
michael <michael.utoh@torilo.co.uk>
2021-12-31 23:38:17 +0100

    Hero page layout

--- components/Main/Hero.js
+++ components/Main/Hero.js
@@ -10,47 +10,34 @@ const HeroComponent = () => {
             >
                 <div className="flex flex-col items-center justify-center lg:w-full">
                     <p className="font-medium font-one text-5xl text-white">Fullsnack Developer Extraordinaire</p>
-                    <p className="font-medium mt-2 text-md text-gray-400 w-8/12 text-center">Started my professional coding journey 2 years ago, working with JavaScript & Python</p>
+                    <p className="font-thin mt-2 text-md text-gray-400 w-8/12 text-center">
+                        Started my professional coding journey 2 years ago, working with JavaScript & Python(Django)
+                    </p>
                 </div>
             </motion.div>
             <motion.button
                 animate="visible"
                 initial="hidden"
-                variants={{
-                    hidden: {
-                        scale: .8,
-                        opacity: 0
-                    },
-                    visible: {
-                        scale: 1,
-                        opacity: 1,
-                        transition: {
-                            delay: 1.5
-                        }
-                    }
-                }}
-                whileHover={{ 
-                    scale:1.2,
-                    transition: { duration: 0.2 },
-                }}
+                variants={{ hidden: { scale: .8, opacity: 0 }, visible: { scale: 1, opacity: 1, transition: { delay: 1.5 }}}}
+                whileHover={{ scale:1.2, transition: { duration: 0.2 }, }}
                 whileTap={{ scale: 0.9 }}
             >
-                <button className="bg-blue-700 font-bold mt-3 px-10 py-2 text-sm text-white">Get Started</button>
+                <button className="bg-purple-500 font-bold mt-4 px-10 py-1 rounded-sm text-sm text-white">Get Started</button>
             </motion.button>
 
             {/* Cards */}
             <div className="flex justify-between lg:w-8/12 mt-20">
 
-                <div className="card flex flex-col items-center justify-center h-60 m-2 text-white w-64">
-                    Hello world
+                <div className="card flex flex-col font-one items-center hover:skew-x-[6deg] justify-center h-60 m-2 text-white w-64">
+                    <p className="text-lg w-10/12 text-center">Autohub Project</p>
                 </div>
 
-                <div className="card flex flex-col items-center justify-center h-60 m-2 text-white w-64">
-                    Hello world
+                <div className="card flex flex-col font-one items-center justify-center h-60 m-2 text-white w-64">
+                    <p className="text-lg w-10/12 text-center">The SideBrief Project</p>
                 </div>
 
-                <div className="card flex flex-col items-center justify-center h-60 m-2 text-white w-64">
-                    Hello world
+                <div className="card flex flex-col font-one items-center hover:-skew-x-[6deg] transform-gpu justify-center h-60 m-2 text-white w-64">
+                    <p className="text-lg w-10/12 text-center">IQ School Management Application</p>
                 </div>
             </div>
         </div>
