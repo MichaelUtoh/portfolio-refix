ac885a613c899d72ce32937ae2f3aeb1d48e64ea
michael <michaelutoh21@gmail.com>
2022-01-31 23:57:46 +0100

    Add content to about page

--- components/Sub/storyline.js
+++ components/Sub/storyline.js
@@ -5,7 +5,7 @@ const StoryBox = () => {
         <>
             <div className="flex flex-col items-center py-20">
                 <p className="text-white">
-                    I am Michael, A Web Developer from Lagos, I speak Python and JavaScript
+                    I am Michael, A Web Developer from Lagos, I write Python and JavaScript Languages, Using Django Rest Framework & React Library along the way.
                 </p>
             </div>
         </>
