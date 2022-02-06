83d1652af742dfc0d4b1e51dfb9d15141e7f7b0b
michael <michaelutoh21@gmail.com>
2022-02-06 19:45:39 +0100

    Fix hamburger link routing

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import NavBar from "../components/Navigation/sideBar";
 import Header from "../components/Navigation/Header"
 import HeroComponent from "../components/Main/homepage";
 import HambergerMenu from "../components/Utils/menu";
+import Link from "next/link";
 
 
 export default function Home() {
@@ -16,7 +17,7 @@ export default function Home() {
 
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="absolute cursor-pointer hamburger h-6 right-10 text-green-300 top-7 w-6 z-20"
+            className="absolute cursor-pointer hamburger h-6 right-10 text-green-400 top-7 w-6 z-20"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -27,12 +28,19 @@ export default function Home() {
 
           <div
             className={showMenu ?
-              "bg-[#444] flex flex-col hamburger-menu-show h-32 items-center justify-center p-4 w-full z-10" :
-              "bg-[#444] flex flex-col hamburger-menu-hide h-32 items-center justify-center p-4 w-full z-10"
+              "bg-[#212121] flex flex-col hamburger-menu-show h-screen items-center justify-start m-2 p-4 rounded-md w-11/12 z-10" :
+              "bg-[#212121] flex flex-col hamburger-menu-hide h-screen items-center justify-start m-2 p-4 rounded-md w-11/12 z-10"
             }
           >
-            <span>Home</span>
-            <span>About</span>
+            <div className="bg-[#444] flex flex-col h-32 items-center justify-center rounded-md w-full">
+              <Link href="/" passHref={false}>
+                <span className="cursor-pointer my-2 text-green-400">Home</span>
+              </Link>
+
+              <Link href="/about" passHref={false}>
+                <span className="cursor-pointer my-2 text-green-400">About</span>
+              </Link>
+            </div>
           </div>
 
         </div>
