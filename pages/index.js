657b2dd3d0d6f061e5d566332c32d6773beab645
michael <michaelutoh21@gmail.com>
2022-02-06 18:47:27 +0100

    Show/hide hamburger menu

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import NavBar from "../components/Navigation/sideBar";
 import Header from "../components/Navigation/Header"
 import HeroComponent from "../components/Main/homepage";
@@ -5,19 +6,36 @@ import HambergerMenu from "../components/Utils/menu";
 
 
 export default function Home() {
+
+  const [showMenu, setShowMenu] = useState()
+
   return (
     <>
       <div className="flex bg-[#212121] relative">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="absolute cursor-pointer hamburger h-6 right-10 text-green-300 top-7 w-6 z-10"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          onClick={() => console.log("Homepage")}
-        >
+        <div className="hamburger-wrap">
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="absolute cursor-pointer hamburger h-6 right-10 text-green-300 top-7 w-6 z-20"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            onClick={() => setShowMenu(!showMenu)}
+          >
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
-        </svg>
+          </svg>
+
+          <div
+            className={showMenu ?
+              "bg-[#444] flex flex-col hamburger-menu-show h-32 items-center justify-center p-4 w-full z-10" :
+              "bg-[#444] flex flex-col hamburger-menu-hide h-32 items-center justify-center p-4 w-full z-10"
+            }
+          >
+            <span>Home</span>
+            <span>About</span>
+          </div>
+
+        </div>
         <Header title="Home" />
         <NavBar />
         <HeroComponent />
